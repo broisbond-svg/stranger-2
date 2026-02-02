866826ed69042d5dd42e687c9db81109001de0a0
broisbond-svg <broisbond@gmail.com>
2026-02-02 01:42:23 -0800

    2fargetuna1.js

--- fargetuna1.js
+++ fargetuna1.js
@@ -1,8 +1,9 @@
 elements.Stranger_Gate = {
     color: ["#4a0000", "#300000", "#1a0000", "#000000"],
     behavior: behaviors.WALL,
-    category: "Land",
+    category: "land",
     state: "solid",
     density: 1200
 };
+
 

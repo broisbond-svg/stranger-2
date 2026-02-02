16925e6a0e1f6d8cbcc901ac3d7c4b1eef2d0e39
broisbond-svg <broisbond@gmail.com>
2026-02-02 01:30:54 -0800

    Add files via upload

---
+++ fargetuna1.js
@@ -0,0 +1,7 @@
+elements.Stranger_Gate = {
+    color: ["#4a0000", "#300000", "#1a0000", "#000000"],
+    behavior: behaviors.SOLID,
+    category: "Solids",
+    state: "solid",
+    density: 1200
+};
